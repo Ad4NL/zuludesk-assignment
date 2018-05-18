b3f1f2d030b5c8cc3d181b81df0611ba305352a1
Marc de Jonge <marc@zuludesk.com>
2018-05-18 09:59:10 +0200

    Change to implementing a function

--- assignment1.php
+++ assignment1.php
@@ -1,13 +1,14 @@
 <?php
 
-$input = [1, 2, 3, 5, 4, 6, 8, 9, 9, 10];
-$output = [];
+/**
+ * Split the input every time the number doesn't increase
+ *
+ * @param int[] $input The input array of numbers
+ * @return int[][] The result should be an array of arrays, for which each array only has increasing numbers and is never empty
+ */
+function split_increasing(array $input): array
+{
+    // TODO Code here
+}
 
-// Split the input every time the number doesn't increase
-// The result should be an array of arrays, for which each array only has increasing numbers and is never empty
-
-// Code here
-
-// End
-
-var_dump( $output );
+var_dump(split_increasing([1, 2, 3, 5, 4, 6, 8, 9, 9, 10]));
