02da0fa7bf21d7395a84853aed2086bdf4845183
Marc de Jonge <marc@zuludesk.com>
2018-05-18 09:56:14 +0200

    First assignment added

---
+++ assignment1.php
@@ -0,0 +1,13 @@
+<?php
+
+$input = [1, 2, 3, 5, 4, 6, 8, 9, 9, 10];
+$output = [];
+
+// Split the input every time the number doesn't increase
+// The result should be an array of arrays, for which each array only has increasing numbers and is never empty
+
+// Code here
+
+// End
+
+var_dump( $output );
