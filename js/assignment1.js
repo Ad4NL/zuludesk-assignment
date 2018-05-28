124be5947dcc78981c175015884f9947117150fe
Marc de Jonge <marc@zuludesk.com>
2018-05-28 15:32:50 +0200

    Add JS version

---
+++ js/assignment1.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     * split the input every time the number doesn't increase
+     *
+     * @param input The input array of numbers
+     * @return The result should be an array of arrays, for which each array only has increasing numbers and is never empty
+     */
+    splitIncreasing: function (input) {
+        // TODO
+    }
+};
